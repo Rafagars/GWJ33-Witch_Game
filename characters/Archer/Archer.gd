e14a74dac922d23a78f0a5b6bd0dc361125f0cf0
rafagars <rafagar96@gmail.com>
2021-05-18 09:31:29 -0400

    Archer fire

--- characters/Archer/Archer.gd
+++ characters/Archer/Archer.gd
@@ -1,18 +1,31 @@
 extends RigidBody2D
 
+var arrow_scene = preload("res://characters/Archer/arrow/Arrow.tscn")
+onready var player = get_parent().get_node("Player")
 export var health = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var target = Vector2(self.position.x, self.position.y)
 	$Move_tween.interpolate_property(self, "position", position, target, 4, Tween.TRANS_QUINT, Tween.EASE_OUT)
-	
-
-
+	$Shoot.start()
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	position.y += 120 * delta
-
+	#position.y += 120 * delta
+	pass
+	
+func spawn_arrows():
+	var arrow = arrow_scene.instance()
+	arrow.position = self.position
+	arrow.dir = Vector2(0, self.position.y).normalized()
+	
+	get_parent().add_child(arrow)
 
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
+
+
+func _on_Shoot_timeout():
+	if health > 0:
+		$AnimatedSprite.play("Fire")
+		spawn_arrows()
--- characters/Player/Player.gd
+++ characters/Player/Player.gd
@@ -33,3 +33,4 @@ func _physics_process(delta):
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 		
 	velocity = move_and_slide(velocity)
+	
